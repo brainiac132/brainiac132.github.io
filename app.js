5ec16e3593184169aad78ef2696cd764126871ca
Ryan Wire <brainiac132@yahoo.com>
2021-05-12 03:12:26 -0400

    Shuffle and Deal Functions added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const SUITS = ['S', 'H', 'D', 'C']
 const RANK = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', "Q", "K"]
 const DECK = []
+const playerHand = []
+const dealerHand = []
 
 class Card {
     constructor(suit, rank, value, altValue = 0) {
@@ -32,9 +34,40 @@ for (let i = 0; i < SUITS.length; i++) {
 console.log(DECK)
 
 
-//Create a function to shuffle the cards
+//Create a function to shuffle the cards (Reference: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array)
 
-// function shuffleDeck() {
-//     DECK.sort(Math.floor(Math.random() * DECK.length))
-//     console.log(DECK)
-// }
+
+function shuffle(array) {
+
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random
+            () * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+
+    }
+    return array;
+
+};
+shuffle(DECK)
+console.log(DECK)
+
+//create a function to deal cards 
+function dealHand() {
+    if (playerHand.length == 0 && dealerHand.length == 0) {
+        for (let i = 1; i <= 4; i++) {
+            let cardToDeal = DECK.pop();
+            if (i % 2 == 0) {
+                playerHand.push(cardToDeal);
+                continue;
+            }
+            dealerHand.push(cardToDeal)
+        }
+    }
+}
+console.log(dealHand())
+//create a function to connect cards to a div element
